991ce8563e5827720eda0a8bea716a92be001ed5
Hayden Chudy <hjc1710@gmail.com>
2011-10-30 23:50:26 -0500

    Adding both files

---
+++ client.py
@@ -0,0 +1,35 @@
+from socket import *
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+while 1:
+    HOST = raw_input("Please enter a host name or exit to terminate the client.\n")
+    HOST = gethostbyname(HOST)
+
+    if HOST == 'exit':
+        break
+
+    PORT = int(raw_input("Please enter a port to connect to.\n"))
+#HOST = 'localhost'
+#PORT = 21566
+    BUFSIZ = 1024
+    ADDR = (HOST, PORT)
+
+    try:
+        tcpCliSock.connect(ADDR)
+        print 'connected to: ' + HOST + ' on ' + str(PORT)
+
+    except error as e:
+        print e
+        print "cannot connect to " + HOST + ' on ' + str(PORT)
+        continue
+    while 1:
+        data = raw_input()
+
+        tcpCliSock.send(data)
+        
+        if data == 'exit':
+            break
+        
+        new_dat = tcpCliSock.recv(BUFSIZ)
+        print new_dat
+    tcpCliSock.close()
---
+++ server.py
@@ -0,0 +1,35 @@
+# Echo server program
+import socket
+import os
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 50007
+print 'Server hosted on ' + str(HOST) + ' Port ' + str(PORT)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+
+while 1:
+    print 'Waiting for Connection....'
+    conn, addr = s.accept()
+    print 'Connected by', addr
+    workingdir = r'C:\Python27'
+    
+    while 1:
+        data = conn.recv(1024)
+
+        if data == 'exit':
+            break
+        elif not data:
+            break
+        elif data == 'ls':
+            dirs = os.listdir(workingdir)
+            the_dirs = "\n".join(dirs)
+            conn.send(the_dirs)
+
+        elif data == 'os':
+            conn.send('os is ' + os.name)
+            
+        conn.send(data)
+    conn.close()
+    print 'Disconnected by', addr
