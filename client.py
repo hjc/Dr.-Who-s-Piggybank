c4dfa38ec32f7533bc2641be8770e3798157ca6c
Hayden Chudy <hjc1710@gmail.com>
2011-12-04 19:25:09 -0600

    go fuck yourself

--- client.py
+++ client.py
@@ -8,10 +8,13 @@ def get_file_name(path):
 
 
 from socket import *
-import os, getpass, math
+import os, getpass, math, re
+from stat import *
 
 #need to make it so ls can accept arguments (done server-side)
 #tcpCliSock = socket(AF_INET, SOCK_STREAM)
+
+splitter = re.compile('\!@\!')
 
 while 1:
     tcpCliSock = socket(AF_INET, SOCK_STREAM)
@@ -82,34 +85,113 @@ while 1:
                         file_path = os.getcwd() + '\\' + file_path
                     else:
                         file_path = os.getcwd() + '/' + file_path
-                sender = open(file_path).read()
-                f = os.stat(file_path)
-                f_size = f.st_size * 8.0
-                #print f_size
-                packets = math.ceil(f_size / BUFSIZ)
+                        
+                try:
+                    sender = open(file_path).read()
+                    
+                except:
+                    print 'File located at: ' + file_path + ' not found.'
+                    continue
+
                 file_descriptor = 'put FN:' + get_file_name(file_path)
-                file_descriptor += '!@!'
-                file_descriptor += 'PACKETS:'
-                file_descriptor += str(int(packets))
                 tcpCliSock.send(file_descriptor)
                 
-                for i in range(0, int(packets)):
-                    tcpCliSock.send(sender[i*BUFSIZ:(i + 1) * BUFSIZ])
-                #print file_descriptor
+                tcpCliSock.send(sender)
+                tcpCliSock.send('>>>~~FILE~~DONE~~<<<')
                 
-                #print sender
+            
+            elif data[0:3] == 'get':
+                file_wanted = data[4:]
+                fd = 'get FN:' + file_wanted
+                tcpCliSock.send(fd)
+                
+                new_dat = tcpCliSock.recv(BUFSIZ)
+                if (new_dat[0:5] == 'Error'):
+                    print new_dat
+                else:
+                    f_info = splitter.split(new_dat)
+                    fn = f_info[0][7:]
+                    fn = get_file_name(fn)
+                    print fn
+                    #packets = f_info[1][8:]
+                    #print packets
+                    
+                    file_data = ''
+                    done = False
+                    
+                    while (not done):
+                        dat = tcpCliSock.recv(BUFSIZ)
+                        if '>>>~~FILE~~DONE~~<<<' in dat:
+                            dat = dat.replace('>>>~~FILE~~DONE~~<<<', '')
+                            done = True
+                        file_data += dat
+                    #for i in range(0, int(packets)):
+                    #    file_data += tcpCliSock.recv(BUFSIZ)
+                    
+                    if '.' not in fn:
+                        fn += '.txt'
+                    
+                    
+                    writer = open(fn, 'w')
+                    writer.write(file_data)
+                    writer.close()
+                    
+                    print fn + ' received successfully'
+                    #print file_data
             
             elif data[0:4] == 'mput':
-                files = data[4:]
-                if files == '*':
+                files = data[5:]
+                if files[-1] == '*':
+                    file_path = files[:-1]
+                    if not file_path.strip():
+                        file_path = os.getcwd()
+                    contents = os.listdir(file_path)
+                    print contents
+                    files = []
+                    for i in range(0, len(contents)):
+                        if (os.name == 'nt'):
+                            this_path = file_path + '\\' + contents[i]
+                        #    mode = os.stat(file_path + '\\' + contents[i]).st_mode
+                        else:
+                            this_path = file_path + '/' + contents[i]
+                        mode = os.stat(this_path).st_mode
+                        if not S_ISDIR(mode):
+                            files.append(this_path)
                     #will need to query current directory (I guess) and send all files
-                    print 'placeholder'
+                    print files
+                    
                 else:
                     files = files.split()
-                    #now send all files
-                    for i in range(0, len(files)):
-                        sender = open(files[i])
-                        #send sender
+                
+                fd = 'mput FILES:' + str(len(files))
+                tcpCliSock.send(fd)
+                print files
+                
+                #now send all files
+                for i in range(0, len(files)):
+                    tcpCliSock.recv(BUFSIZ)
+                    sender = open(files[i])
+                    file_path = files[i]
+                    if (file_path[0] != "\\" and file_path[0] != "/"):
+                        if (os.name == 'nt'):
+                            file_path = os.getcwd() + '\\' + file_path
+                        else:
+                            file_path = os.getcwd() + '/' + file_path
+                            
+                    try:        
+                        sender = open(file_path).read()
+                        
+                    except:
+                        print 'File located at: ' + file_path + ' not found. Ignoring \
+                        and moving on.'
+    
+                    file_descriptor = 'put FN:' + get_file_name(file_path)
+                    print file_descriptor
+                    tcpCliSock.send(file_descriptor)
+                    
+                    tcpCliSock.send(sender)
+                    tcpCliSock.send('>>>~~FILE~~DONE~~<<<')
+                    #send sender
             
             #needs to become quit
             elif data == 'exit':
