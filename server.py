1d68d0cc648abf7210acf302826efdd7924c5107
Jonathan Stewart <jbstewart5@gmail.com>
2011-12-06 17:44:42 -0600

    Fixed Compression and Encryption

--- server.py
+++ server.py
@@ -1,6 +1,73 @@
 #from Crypto.Cipher import AES
-#import os,random, struct
-import zlib
+import os,random, struct
+#import zlib
+import gzip
+
+
+
+def encrypt_file(input_string):
+    key = "29"
+    size = len(input_string)
+    if (size%2) != 0:
+        input_string += " "
+    for i in xrange(size/2):
+        key += '29'
+    input_string = "".join(chr(ord(a)^ord(b))for a,b in zip(input_string,key))
+    return input_string
+
+def compress_file(input_string, is_binary = False):
+    #f = open(in_filename,"r")
+    #string = f.read()
+    #f.close()
+    #compr = glib.compress(in_filename)
+    #output = open(out_filename,"w")
+    #output.write(compr)
+    #output.close()
+    #print input_string
+    if is_binary:
+        if os.name =='nt':
+            file_name = os.getcwd() + '\\tempfileftp.gz'
+            tempfile = gzip.open(file_name,'wb')
+            tempfile.write(input_string)
+            tempfile.close()
+        else:
+            file_name = os.getcwd() + '/tempfileftp.gz'
+            tempfile = gzip.open(file_name,'wb')
+            tempfile.write(input_string)
+            tempfile.close()
+    else:
+        if os.name =='nt':
+            file_name = os.getcwd() + '\\tempfileftp.txt.gz'
+            tempfile = gzip.open(file_name ,'wb')
+            #tempfile = gzip.open('test.txt','wb')
+            tempfile.write(input_string)
+            tempfile.close()
+        else:
+            file_name = os.getcwd() + '/tempfileftp.txt.gz'
+            tempfile = gzip.open(file_name,'wb')
+            tempfile.write(input_string)
+            tempfile.close()
+    #print open(file_name).read()
+    #file_name = 'test.txt'    
+    return file_name
+
+#Function inputs a compressed file and outputs a string
+def decompress_file(in_file):
+    #f = open(in_filename,"r")
+    #string = f.read()
+    #f.close()
+    #decomp = zlib.decompress(in_string)
+    #output = open(out_filename,"w")
+    #output.write(decomp)
+    #output.close()
+    tempfile = gzip.open(in_file, 'rb')
+    output_string = tempfile.read()
+    tempfile.close()
+    os.remove(in_file)
+    return output_string
+
+
+
 #
 #def encrypt_file(in_filename, out_filename=None, chunksize=64*1024):
 #    """ Encrypts a file using AES (CBC mode) with the
@@ -85,25 +152,25 @@ import zlib
 #
 #            #outfile.truncate(origsize)
 #
-def compress_file(in_filename):
-    #f = open(in_filename,"r")
-    #string = f.read()
-    #f.close()
-    compr = zlib.compress(in_filename)
-    #output = open(in_filename,"w")
-    #output.write(compr)
-    #output.close()
-    return compr
-        
-def decompress_file(in_string):
-    #f = open(in_filename,"r")
-    #string = f.read()
-    #f.close()
-    decomp = zlib.decompress(in_string)
-    #output = open(in_filename,"w")
-    #output.write(decomp)
-    #output.close()
-    return decomp
+#def compress_file(in_filename):
+#    #f = open(in_filename,"r")
+#    #string = f.read()
+#    #f.close()
+#    compr = zlib.compress(in_filename)
+#    #output = open(in_filename,"w")
+#    #output.write(compr)
+#    #output.close()
+#    return compr
+#        
+#def decompress_file(in_string):
+#    #f = open(in_filename,"r")
+#    #string = f.read()
+#    #f.close()
+#    decomp = zlib.decompress(in_string)
+#    #output = open(in_filename,"w")
+#    #output.write(decomp)
+#    #output.close()
+#    return decomp
 
 
 
@@ -119,7 +186,7 @@ def get_file_name(path):
 
 
 def grab_domain(m):
-    email_re = re.compile('^[\w+\-.]+@[a-z\d\-]+\.(?P<domain>[a-z.]+)+$')
+    email_re = re.compile('^[\w+\-.]+@[A-Za-z\d\-]+\.(?P<domain>[a-z.]+)+$')
     
     matches = re.match(email_re, m)
     
@@ -142,6 +209,14 @@ import os
 import re
 from math import ceil
 from stat import *
+
+#datum = open('pg76.txt').read()
+#temp_fn = compress_file(datum)
+#print open(temp_fn, 'rb').read()
+#raw_input()
+#string2 = decompress_file(temp_fn)
+#
+#print string2
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 50007
@@ -189,7 +264,8 @@ while 1:
         data = conn.recv(buffsize)
         
         # Exit
-        if data == 'exit':
+        if data == 'exit' or data == 'quit':
+            conn.send('Goodbye!')
             break
         elif not data:
             break
@@ -226,10 +302,22 @@ while 1:
                     done = True
                 
                 f += dat
-            if ENCRYPT:
-                f = decrypt_file(f)
+            
             if COMPRESS:
-                f = decompress_file(f)
+                if os.name == 'nt':
+                    temp_fp = os.getcwd() + '\\temp_file.gzip'
+                else:
+                    temp_fp = os.getcwd() + '/temp_file.gzip'
+                temp = open(temp_fp, 'wb')
+                temp.write(f)
+                temp.close()
+                f = decompress_file(temp_fp)
+                #print f
+                #os.remove(temp_fp)
+            
+            if ENCRYPT:
+                f = encrypt_file(f)
+                
             if BINARY:
                 stor = open(fn, 'wb')
             else:
@@ -242,7 +330,10 @@ while 1:
         elif data[0:3] == 'get':
             f = data[7:]
             try:
-                f_open = open(f).read()
+                if BINARY:
+                    f_open = open(f,'rb').read()
+                else:
+                    f_open = open(f).read()
             except:
                 print 'Error ' + f + ' not found'
                 conn.send('Error ' + f + ' not found')
@@ -256,14 +347,25 @@ while 1:
                 file_path = f
             fd = 'get FN:' + f
             conn.send(fd)
+            
+            if ENCRYPT:
+                print 'First fifteen characters of file before', f_open[0:15]
+                f_open = encrypt_file(f_open)
+                print 'First fifteen characters of file after', f_open[0:15]
+            
             if COMPRESS:
                 print 'Length of file before compression: ', len(f_open)
-                f_open = compress_file(f_open)
+                compressed_file_name = compress_file(f_open)
+                f_open = open(compressed_file_name, 'rb').read()
+                #print f_open
                 print 'Length of file after compression: ', len(f_open)
             conn.send(f_open)
             conn.send('>>>~~FILE~~DONE~~<<<')
             
             print f + ' successfully sent'
+            
+            if COMPRESS:
+                os.remove(compressed_file_name)
             
         # MPUT        
         elif data[0:4] == 'mput':
@@ -289,7 +391,18 @@ while 1:
                     
                     f += dat
                 if COMPRESS:
-                    f = decompress_file(f) 
+                    if os.name == 'nt':
+                        temp_fp = os.getcwd() + '\\temp_file.gzip'
+                    else:
+                        temp_fp = os.getcwd() + '/temp_file.gzip'
+                    temp = open(temp_fp, 'wb')
+                    temp.write(f)
+                    temp.close()
+                    f = decompress_file(temp_fp) 
+                
+                if ENCRYPT:
+                    f = encrypt_file(f)
+                
                 if BINARY:
                     stor = open(fn, 'wb')
                 else:
@@ -330,7 +443,10 @@ while 1:
                     else:
                         file_path = os.getcwd() + '/' + file_path
                 try:
-                    sender = open(file_path).read()
+                    if BINARY:
+                        sender = open(file_path, 'rb').read()
+                    else:
+                        sender = open(file_path).read()
                 except:
                     print 'File located at: ' + file_path + ' not found.  Ignoring \
                     and moving on.'
@@ -339,13 +455,26 @@ while 1:
                 fn = get_file_name(file_path)
                 print file_descriptor
                 conn.send(file_descriptor)
-                if COMPRESS:
-                    print 'Length of file: ', fn, 'is:', len(sender), 'before compression: '
-                    sender = compress_file(sender)
-                    print 'Length of file: ', fn, 'is:', len(sender), 'after compression: '
                 
+                if ENCRYPT:
+                    print 'First fifteen characters of file before', sender[0:15]
+                    sender = encrypt_file(sender)
+                    print 'First fifteen characters of file after', sender[0:15]
+                    
+                if COMPRESS:
+                    print 'Length of file before compression: ', len(sender)
+                    compressed_file_name = compress_file(sender)
+                    sender = open(compressed_file_name, 'rb').read()
+                    #print f_open
+                    print 'Length of file after compression: ', len(sender)
                 conn.send(sender)
                 conn.send('>>>~~FILE~~DONE~~<<<')
+                print fn + ' successfully sent'
+                                
+                if COMPRESS:
+                    os.remove(compressed_file_name)
+                    
+                
                 
         # COMPRESSION ON/OFF                
         elif data == 'compress':
@@ -375,6 +504,11 @@ while 1:
         elif data == 'binary':
             BINARY = True
             print 'Binary mode on'
+            
+        #ASCII ON/OFF        
+        elif data == 'ascii':
+            BINARY = False
+            print 'ASCII mode on'
             
     conn.close()
     print 'Disconnected by', addr
